7bb4418df1e9f9b717621d6eeadea9bbd2b1114c
kristian <kristian.dimitrov@alfresco.com>
2020-07-02 17:18:24 +0100

    ACA-100: Cherry-pick e2e commit

--- lib/testing/src/lib/core/pages/viewer.page.ts
+++ lib/testing/src/lib/core/pages/viewer.page.ts
@@ -15,6 +15,24 @@
  * limitations under the License.
  */
 
+
+/*!
+ * @license
+ * Copyright 2019 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { BrowserActions } from '../utils/browser-actions';
 import { TabsPage } from './material/tabs.page';
 import { TogglePage } from './material/toggle.page';
@@ -637,5 +655,9 @@ export class ViewerPage {
     async getUnknownFormatMessage(): Promise<string> {
         const unknownFormatLabel = this.unknownFormat.element(by.css(`.adf-viewer__unknown-label`));
         return BrowserActions.getText(unknownFormatLabel);
+    }
+
+    async expectUrlToContain(text: string): Promise<void> {
+        await expect(browser.getCurrentUrl()).toContain(text);
     }
 }
