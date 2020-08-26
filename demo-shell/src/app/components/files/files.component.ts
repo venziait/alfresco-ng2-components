afc546f351267256dd9e9058985a70ce61111dfd
Cano <david.cano.nieto@gmail.com>
2020-08-26 09:37:04 +0100

    Second commit

--- demo-shell/src/app/components/files/files.component.ts
+++ demo-shell/src/app/components/files/files.component.ts
@@ -164,7 +164,7 @@ export class FilesComponent implements OnInit, OnChanges, OnDestroy {
     navigationRoute = '/files';
 
     @Input()
-    enableCustomHeaderFilter = false;
+    headerFilters = false;
 
     @Input()
     paramValues: Map<any, any> = null;
@@ -640,33 +640,33 @@ export class FilesComponent implements OnInit, OnChanges, OnDestroy {
         return '';
     }
 
-    onFilterUpdate(newNodePaging: NodePaging) {
-        this.nodeResult = newNodePaging;
-    }
+    // onFilterUpdate(newNodePaging: NodePaging) {
+    //     this.nodeResult = newNodePaging;
+    // }
 
-    onAllFilterCleared() {
-        this.documentList.node = null;
-        if (this.currentFolderId === '-my-') {
-            this.router.navigate([this.navigationRoute, '']);
-        } else {
-            this.router.navigate([this.navigationRoute, this.currentFolderId, 'display', this.displayMode]);
-        }
-        this.documentList.reload();
-    }
+    // onAllFilterCleared() {
+    //     this.documentList.node = null;
+    //     if (this.currentFolderId === '-my-') {
+    //         this.router.navigate([this.navigationRoute, '']);
+    //     } else {
+    //         this.router.navigate([this.navigationRoute, this.currentFolderId, 'display', this.displayMode]);
+    //     }
+    //     this.documentList.reload();
+    // }
 
-    onFilterSelected(currentActiveFilters: Map<string, string>) {
-        const objectFromMap = {};
-        currentActiveFilters.forEach((value: any, key) => {
-            let paramValue = null;
-            if (value && value.from && value.to) {
-                paramValue = `${value.from}||${value.to}`;
-            } else {
-                paramValue = value;
-            }
-            objectFromMap[key] = paramValue;
-        });
+    // onFilterSelected(currentActiveFilters: Map<string, string>) {
+    //     const objectFromMap = {};
+    //     currentActiveFilters.forEach((value: any, key) => {
+    //         let paramValue = null;
+    //         if (value && value.from && value.to) {
+    //             paramValue = `${value.from}||${value.to}`;
+    //         } else {
+    //             paramValue = value;
+    //         }
+    //         objectFromMap[key] = paramValue;
+    //     });
 
-        this.router.navigate([], { relativeTo: this.route, queryParams: objectFromMap });
-    }
+    //     this.router.navigate([], { relativeTo: this.route, queryParams: objectFromMap });
+    // }
 
 }
